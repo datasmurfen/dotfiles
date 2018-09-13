f93bc9cd3255064018b999d340aa28149695fbf4
Oddvar Tengesdal <oddvar.tengesdal@hotmail.com>
2018-09-13 20:08:55 +0200

    autobackup dotfiles on 2018-09-13 20:08:55

---
+++ backupDotfiles.sh
@@ -0,0 +1,22 @@
+
+
+rm -rf .i3/*
+cp -r /home/oddvar/.config/i3/* /home/oddvar/dotfiles/.i3/          
+
+
+
+rm -rf .zshrc
+cp -r /home/oddvar/.zshrc /home/oddvar/dotfiles/         
+
+
+
+
+rm -rf .gitconfig
+cp -r /home/oddvar/.gitconfig /home/oddvar/dotfiles/         
+
+
+
+
+git add . 
+git commit -m "autobackup dotfiles on `date +'%Y-%m-%d %H:%M:%S'`";
+git push origin master 
