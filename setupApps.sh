b45f0ca77dc5e13f82a28b8c15edcd83e206661f
Oddvar Tengesdal <oddvar.tengesdal@hotmail.com>
2018-09-13 21:09:10 +0200

    autobackup dotfiles on 13-09-2018 21:09:10

---
+++ setupApps.sh
@@ -0,0 +1,48 @@
+#Android Studio
+#sudo mkdir /opt/tmp
+#sudo chmod 777 /opt/tmp
+#export TMPDIR=/opt/tmp
+#yaourt -S android-studio
+
+
+#Install Vim
+#sudo pacman -S vim
+
+# Install Terminator 
+#sudo pacman -S terminator 
+
+# Install Yaourt 
+#sudo pacman -S yaourt
+
+#Install Google Chrome 
+#yaourt -S google-chrome
+
+#Zsh - open in terminator, and run zsh to setup
+#pacman -S zsh
+
+#Change shell 
+#chsh -s $(which zsh)
+
+## I3
+#sudo pacman -S i3
+
+## PlayerCtl
+##sudo pacman -S playerctl
+
+#Feh 
+# sudo pacman -S feh
+
+#Arandr
+#sudo pacman -S arandr
+
+#Rofi 
+#sudo pacman -S rofi
+
+#Visual Studio Code
+#yaourt -S visual-studio-code-bin
+
+#Discord
+#yaourt -S discord-canary
+
+#Spotify
+#yaourt -S spotify
