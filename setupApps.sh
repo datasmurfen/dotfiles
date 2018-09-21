57026fed4736ee4e7cce287431e7cbb8b6da58f5
Oddvar Tengesdal <oddvar.tengesdal@hotmail.com>
2018-09-21 22:23:50 +0200

    autobackup dotfiles on 21-09-2018 22:23:50

--- setupApps.sh
+++ setupApps.sh
@@ -52,3 +52,17 @@
 ## Oh-my-zsh
 #sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
+## NPM 
+#sudo pacman -S npm
+
+## Node JS 
+#npm install nodejs -g 
+
+## Ionic Cordova 
+#npm install ionic cordova -g
+
+
+## Gradle 
+#sudo pacman -S gradle
+
+
